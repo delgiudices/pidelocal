c1b8d20c8e792ac9b2f4ad82d2983384291574a3
Luis Del Giudice <luis.dg19@gmail.com>
2016-03-07 15:23:16 -0400

    Removed breakpoint

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,6 @@ class HomeController < ApplicationController
     store_request_params = params.require(:store_request).permit(:full_name, :email, :store_name, :phone, :store_phone, :password, :store_address)
     store_request = StoreRequest.new store_request_params
     if store_request.save
-      byebug
       StoreNotifier.create_store_request_email(store_request).deliver
       redirect_to store_request_successful_path
     end
